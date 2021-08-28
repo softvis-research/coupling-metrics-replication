082070a00b631a0d32e760c176a08f7112b08791
rmllr <rmueller@wifa.uni-leipzig.de>
2021-08-28 19:34:48 +0200

    remove system

--- replication/experiment/4/system/bin/display-help.bat
+++
@@ -1,4 +0,0 @@
-echo Usage: start-jira.bat [OPTION]
-echo     /fg                         Start JIRA in the foreground
-echo     /disableaddons=ADDONS       Start JIRA with ADDONS disabled, where ADDONS is a colon-separated list of addon keys
-echo     /disablealladdons           Start JIRA with all user installed addons disabled
--- replication/experiment/4/system/bin/stop-jira.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set _PRG_DIR=%~dp0
-"%_PRG_DIR%/shutdown.bat" %1 %2 %3 %4 %5 %6 %7 %8 %9
